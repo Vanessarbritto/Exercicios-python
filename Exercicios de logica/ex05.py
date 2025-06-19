a46bb840528492c102feab7a66de878ce7652eb6
Vanessa Brito <nnessa.rbrito003@gmail.com>
2025-06-19 19:12:05 -0300

    renomeando exercicios, adicionando nova pasta

---
+++ Exercicios de logica/ex05.py
@@ -0,0 +1,9 @@
+# 5- Entrar via teclado com o valor de uma temperatura em graus Celsius, 
+# calcular e exibir sua temperatura equivalente em Fahrenheit
+
+# FUNÇÃO #
+
+celsius = float(input("digite a temperatura em graus celsius:"))
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"a temperatura em Fahrenheit é: {fahrenheit} ºF:!")
