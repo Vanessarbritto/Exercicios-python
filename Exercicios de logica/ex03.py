48034bd28ddad484046948fe36df570a8ee23649
Vanessa Brito <nnessa.rbrito003@gmail.com>
2025-06-19 20:23:51 -0300

    Update ex03.py

--- Exercicios de logica/ex03.py
+++ Exercicios de logica/ex03.py
@@ -1,3 +1,6 @@
+# 3 - apartir dos valores da base e altura 
+#de um triangulo, calcular e exibir sua area
+
 base = int(input("digite a base do triangulo: "))
 altura = float(input("digite a altura do triangulo: "))
 
